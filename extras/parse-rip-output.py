c03e9037f87886308856b7a3bd2e2968f5a85ae9
Slate Gray <6736580-SlateGray@users.noreply.gitlab.com>
2022-09-27 18:51:21 -0500

    Format, Lint, Update Ignored Rules/Files
    Formatted files with black and brought up to flake8 standards.
    Changed parse to use double spacing instead of tabs.
    Added output files to .gitignore and create .flake8 to ignore line
    length errors.

--- extras/parse-rip-output.py
+++ extras/parse-rip-output.py
@@ -1,75 +1,83 @@
 from __future__ import print_function
-import json, markdown, time, sys
+import json
+import markdown
+import time
+import sys
 
 if not len(sys.argv) == 2:
-	print('usage:', sys.argv[0], '[threadfile.json]')
-	print('will output to [threadfile.json].html')
+    print("usage:", sys.argv[0], "[threadfile.json]")
+    print("will output to [threadfile.json].html")
 
-	sys.exit(0)
+    sys.exit(0)
 
-with open(sys.argv[1], 'r') as f:
-	data = json.load(f)[1]
+with open(sys.argv[1], "r") as f:
+    data = json.load(f)[1]
 
-html = '''<!DOCTYPE html>
+html = """<!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>output</title>
-		<style>
-			body {
-				font-family: sans-serif;
-			}
+  <head>
+    <meta charset="UTF-8">
+    <title>output</title>
+    <style>
+      body {
+        font-family: sans-serif;
+      }
 
-			.comment {
-				margin-left: 32px;
-				padding: 4px;
-				border: 1px solid black;
-			}
+      .comment {
+        margin-left: 32px;
+        padding: 4px;
+        border: 1px solid black;
+      }
 
-			.username {
-				color: #22f;
-			}
+      .username {
+        color: #22f;
+      }
 
-			p {
-				font-size: 10pt;
-			}
-		</style>
-	</head>
-	<body>
+      p {
+        font-size: 10pt;
+      }
+    </style>
+  </head>
+  <body>
 %s
-	</body>
+  </body>
 </html>
-'''
+"""
+
 
 def formComment(comment):
-	ret = '<div class="comment"><small><span class="username">%s</span> %s <b>%i</b></small><p>%s</p><!--__REPLIES__--></div>' % (
-		comment['author'],
-		time.ctime(comment['created']),
-		comment['score'],
-		markdown.markdown(comment['body'])
-	)
+    ret = (
+        '<div class="comment"><small><span class="username">%s</span> %s <b>%i</b></small><p>%s</p><!--__REPLIES__--></div>'
+        % (
+            comment["author"],
+            time.ctime(comment["created"]),
+            comment["score"],
+            markdown.markdown(comment["body"]),
+        )
+    )
 
-	return ret
+    return ret
+
 
 def doTheDing(comment):
-	base = formComment(comment)
+    base = formComment(comment)
 
-	if len(comment['replies']) > 0:
-		replies = []
-		for reply in comment['replies']:
-			replies.append(doTheDing(reply))
+    if len(comment["replies"]) > 0:
+        replies = []
+        for reply in comment["replies"]:
+            replies.append(doTheDing(reply))
 
-		base = base.replace('<!--__REPLIES__-->', '\n'.join(replies))
+        base = base.replace("<!--__REPLIES__-->", "\n".join(replies))
 
-	return base
+    return base
 
 
 out = []
 
 for comment in data:
-	out.append(doTheDing(comment))
+    out.append(doTheDing(comment))
 
-html = html % ('\n\n'.join(out),)
+html = html % ("\n\n".join(out),)
 
-with open(sys.argv[1] + '.html','w') as f:
-	f.write(html)
+with open(sys.argv[1] + ".html", "w") as f:
+    f.write(html)
--- ripper.py
+++ ripper.py
@@ -1,107 +1,124 @@
 from __future__ import print_function
-import json, requests, os
+import json
+import requests
 
 sess = requests.Session()
 
+
 def getjson(url):
-	print('downloading',url)
-	ret = sess.get(url, headers={'User-Agent': 'ripperino/1.0'}).json()
-	return ret
+    print("downloading", url)
+    ret = sess.get(url, headers={"User-Agent": "ripperino/1.0"}).json()
+    return ret
+
 
 def parseReplies(topic, toplevelcomments, out):
-	for comment in toplevelcomments:
-		if comment['kind'] == 't1':
-			newitem = {
-				'author': comment['data']['author'],
-				'created': comment['data']['created_utc'],
-				'score': comment['data']['score'],
-				'commentid': comment['data']['id'],
-				'body': comment['data']['body'],
-				'replies': []
-			}
-			out.append(newitem)
+    for comment in toplevelcomments:
+        if comment["kind"] == "t1":
+            newitem = {
+                "author": comment["data"]["author"],
+                "created": comment["data"]["created_utc"],
+                "score": comment["data"]["score"],
+                "commentid": comment["data"]["id"],
+                "body": comment["data"]["body"],
+                "replies": [],
+            }
+            out.append(newitem)
 
-			if 'replies' in comment['data'] and len(comment['data']['replies']) > 0:
-				parseReplies(topic, comment['data']['replies']['data']['children'], newitem['replies'])
+            if "replies" in comment["data"] and len(comment["data"]["replies"]) > 0:
+                parseReplies(
+                    topic,
+                    comment["data"]["replies"]["data"]["children"],
+                    newitem["replies"],
+                )
 
-		elif comment['kind'] == 'more':
-			idarray = [topic['permalink'] + x for x in comment['data']['children']]
-			if len(idarray) == 0:
-				continue
-			print('MORE! grabbing', len(idarray), 'more comments using requests.get')
-			newcomments = [getjson('https://reddit.com' + x + '/.json') for x in idarray]
+        elif comment["kind"] == "more":
+            idarray = [topic["permalink"] + x for x in comment["data"]["children"]]
+            if len(idarray) == 0:
+                continue
+            print("MORE! grabbing", len(idarray), "more comments using requests.get")
+            newcomments = [
+                getjson("https://reddit.com" + x + "/.json") for x in idarray
+            ]
 
-			for camment in newcomments:
-				if len(camment[1]['data']['children']) == 0:
-					continue
-				toplevel = camment[1]['data']['children'][0]['data']
-				responses = toplevel['replies']
+            for camment in newcomments:
+                if len(camment[1]["data"]["children"]) == 0:
+                    continue
+                toplevel = camment[1]["data"]["children"][0]["data"]
+                responses = toplevel["replies"]
 
-				newitem = {
-					'author': toplevel['author'],
-					'created': toplevel['created_utc'],
-					'score': toplevel['score'],
-					'commentid': toplevel['id'],
-					'body': toplevel['body'],
-					'replies': []
-				}
-				out.append(newitem)
+                newitem = {
+                    "author": toplevel["author"],
+                    "created": toplevel["created_utc"],
+                    "score": toplevel["score"],
+                    "commentid": toplevel["id"],
+                    "body": toplevel["body"],
+                    "replies": [],
+                }
+                out.append(newitem)
 
-				if not responses == '':
-					parseReplies(topic, responses['data']['children'], newitem['replies'])
+                if not responses == "":
+                    parseReplies(
+                        topic, responses["data"]["children"], newitem["replies"]
+                    )
+
 
 def countComments(parsed, obj=None):
-	if obj:
-		if not 'commentcount' in obj:
-			obj['commentcount'] = 0
-		obj['commentcount'] += len(parsed)
-	else:
-		obj = {'commentcount': len(parsed)}
-	
-	for comment in parsed:
-		if len(comment['replies']) > 0:
-			countComments(comment['replies'], obj)
-	
-	return obj['commentcount']
+    if obj:
+        if "commentcount" not in obj:
+            obj["commentcount"] = 0
+        obj["commentcount"] += len(parsed)
+    else:
+        obj = {"commentcount": len(parsed)}
+
+    for comment in parsed:
+        if len(comment["replies"]) > 0:
+            countComments(comment["replies"], obj)
+
+    return obj["commentcount"]
+
 
 def downloadCommentsSection(json_url):
-	data = requests.get(json_url, headers={'User-Agent': 'ripperino/1.0'}).json()
-	with open('temporary.json','w') as f:
-		json.dump(data,f)
+    data = requests.get(json_url, headers={"User-Agent": "ripperino/1.0"}).json()
+    with open("temporary.json", "w") as f:
+        json.dump(data, f)
 
-	parsed = []
-	topic = {
-		'author': data[0]['data']['children'][0]['data']['author'],
-		'created': data[0]['data']['children'][0]['data']['created_utc'],
-		'score': data[0]['data']['children'][0]['data']['score'],
-		'permalink': data[0]['data']['children'][0]['data']['permalink'],
-		'url': data[0]['data']['children'][0]['data']['url'],
-		'title': data[0]['data']['children'][0]['data']['title'],
-		'body':  data[0]['data']['children'][0]['data']['selftext'],
-		'id': data[0]['data']['children'][0]['data']['id'],
+    parsed = []
+    topic = {
+        "author": data[0]["data"]["children"][0]["data"]["author"],
+        "created": data[0]["data"]["children"][0]["data"]["created_utc"],
+        "score": data[0]["data"]["children"][0]["data"]["score"],
+        "permalink": data[0]["data"]["children"][0]["data"]["permalink"],
+        "url": data[0]["data"]["children"][0]["data"]["url"],
+        "title": data[0]["data"]["children"][0]["data"]["title"],
+        "body": data[0]["data"]["children"][0]["data"]["selftext"],
+        "id": data[0]["data"]["children"][0]["data"]["id"],
+        "nsfw": data[0]["data"]["children"][0]["data"]["over_18"],
+        "gilded": data[0]["data"]["children"][0]["data"]["gilded"],
+        "stickied": data[0]["data"]["children"][0]["data"]["stickied"],
+        "commentcount": 0,
+    }
 
-		'nsfw': data[0]['data']['children'][0]['data']['over_18'],
-		'gilded': data[0]['data']['children'][0]['data']['gilded'],
-		'stickied': data[0]['data']['children'][0]['data']['stickied'],
-		'commentcount': 0
-	}
+    parseReplies(topic, data[1]["data"]["children"], parsed)
 
-	parseReplies(topic, data[1]['data']['children'], parsed)
+    countComments(parsed, topic)
 
-	countComments(parsed, topic)
+    return topic, parsed
 
-	return topic, parsed
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-	import sys
+    import sys
 
-	if not len(sys.argv) == 2:
-		print('usage:', sys.argv[0], '[reddit thread URL]')
-		sys.exit(0)
+    if not len(sys.argv) == 2:
+        print("usage:", sys.argv[0], "[reddit thread URL]")
+        sys.exit(0)
 
-	topic, parsed = downloadCommentsSection(sys.argv[1] + '.json')
+    topic, parsed = downloadCommentsSection(sys.argv[1] + ".json")
 
-	with open('%s.%s-%i.json' % (topic['id'], topic['permalink'].split('/')[-2], int(topic['created'])),'w') as f:
-		# json.dump([topic, parsed], f, indent=4, separators=(',', ': ')) # pretty print
-		json.dump([topic, parsed], f)
+    with open(
+        "%s.%s-%i.json"
+        % (topic["id"], topic["permalink"].split("/")[-2], int(topic["created"])),
+        "w",
+    ) as f:
+        # json.dump([topic, parsed], f, indent=4, separators=(',', ': ')) # pretty print
+        json.dump([topic, parsed], f)
